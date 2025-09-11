ea621f27eeed606b0625248c0ebaa4fabe39148c
devilra <rockerraja906@gmail.com>
2025-09-11 10:21:42 +0530

    update product

--- frontent/app/api.js
+++ frontent/app/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
-  //baseURL: "https://blinkit-clone-ecommerce.onrender.com/api",
+  //baseURL: "http://localhost:4000/api",
+  baseURL: "https://blinkit-clone-ecommerce.onrender.com/api",
   withCredentials: true, // cookie share
 });
 
